d4a24fd0184d494f4c6b9de1834833bca862f1c4
m513t20 <email431@email431.com>
2024-06-19 20:20:57 +0800

    merge

--- Src/Models/Keyboard.py
+++ Src/Models/Keyboard.py
@@ -1,10 +1,10 @@
 from Src.Models.AbstractModel import AbstractModel
-<<<<<<< HEAD
+
 from dataclasses import dataclass,field
-=======
+
+
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
->>>>>>> 650178d575c8ad5676be9018863a7c93354deaed
 
 @dataclass
 class Keyboard(AbstractModel):
@@ -12,25 +12,9 @@ class Keyboard(AbstractModel):
     Модель клавиатуры бота
     '''
     __id: int
-<<<<<<< HEAD
+
     __data: list[list[int|str]]=field(default_factory=list[list[int|str]])
 
-
-=======
-    __data: list[dict[str,int]]
-
-
-    def __init__(self, id:int, data: list[dict[str,int]]) -> None:
-        '''
-        Модель клавиатуры бота
-
-        Args:
-            id - уникальный номер клавиатуры
-            data - список словарей, где каждый словарь строка, а элементы в нём - текст кнопки и id сообщения к которому она ведёт
-        '''
-        self.__data: list[dict[str,int]] = data
-        self.__id = id
->>>>>>> 650178d575c8ad5676be9018863a7c93354deaed
 
 
     def build_markup(self):
