51ac9cf56849493bb4886371921f2f6469ca86f2
m513t20 <email431@email431.com>
2024-06-19 20:10:49 +0800

    todo

--- Src/Models/__init__.py
+++ Src/Models/__init__.py
@@ -2,3 +2,9 @@ from Src.Models.AbstractModel import AbstractModel
 from Src.Models.Keyboard import Keyboard
 from Src.Models.Message import Message
 from Src.Models.User import User
+#TODO заполнить
+#enums на папку вышк моделей, модели смешать
+#enums - генерим статичным методом
+#enums - статичный гетер
+#классы с логикой - маленькая, модели - большая
+#запулить из мэйна
